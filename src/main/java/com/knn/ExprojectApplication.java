e19cc93817e5edd19001a27d137e643e64f9962f
kangningning <17684855213@163.com>
2022-10-17 01:59:26 +0800

    测试

--- src/main/java/com/knn/ExprojectApplication.java
+++ src/main/java/com/knn/ExprojectApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ExprojectApplication {
 
+//    21
     public static void main(String[] args) {
         SpringApplication.run(ExprojectApplication.class, args);
     }
